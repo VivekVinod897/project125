b6a4f300f28d55a5f7019ce14a9b74a3ec77e489
VivekVinod897 <76860481+VivekVinod897@users.noreply.github.com>
2021-05-25 13:03:14 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,34 @@
+difference = 0;
+rightWristX = 0;
+leftWristX = 0;
+
+function setup(){
+video = createCapture(VIDEO);
+video.size(550,500);
+
+canvas = createCanvas(550,550);
+canvas.position(560,150)
+poseNet = ml5.poseNet(video,modelLoaded);
+poseNet.on('pose', gotPoses);
+}
+
+function draw(){
+background('#008080')
+textSize(leftWristX, rightWristX, difference);
+text('Vivek', 10, 30);
+fill(255, 255, 255);
+}
+
+function modelLoaded(){
+console.log("PoseNet is initialized")
+}
+
+function gotPoses(results){
+if(results.length > 0){
+console.log(results)
+rightWristX = results[0].pose.rightWrist.x;
+leftWristX = results[0].pose.leftWrist.x;
+difference = floor(leftWristX - rightWristX);
+console.log("leftWristX = "+leftWristX+"rightWristX = "+rightWristX+"difference = "+difference);
+}
+}
